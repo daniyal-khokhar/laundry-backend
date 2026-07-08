0c5b1e892fc115a36c46061caf8c2c683eefca53
daniyal-khokhar <daniyalmazhar3363@gmail.com>
2026-07-08 13:29:24 +0500

    now hello work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, Controller, Get } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OrdersModule } from './orders/orders.module';
 
+// 1. Ek chota sa controller yahi bana lete hain root path k liye
+@Controller()
+export class AppController {
+  @Get()
+  getHello(): string {
+    return 'Decent Laundry Backend is running successfully with MongoDB!';
+  }
+}
+
 @Module({
   imports: [
-    // Yeh directly aapki Vercel ya local .env file se connection string uthaye ga
     MongooseModule.forRoot(process.env.MONGO_CON_STRING || 'mongodb://127.0.0.1:27017/decent-laundry'),
     OrdersModule,
   ],
+  controllers: [AppController], // 2. Is controller ko yahan register kar diya
 })
 export class AppModule {}
